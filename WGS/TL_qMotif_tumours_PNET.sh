615db09be54556a2e277f266b3433d5249f47d74
Michael Lee <52021457+mlee1125@users.noreply.github.com>
2019-08-19 16:24:36 +1000

    Upload TL from qMotif scripts

---
+++ WGS/TL_qMotif_tumours_PNET.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#for f in `ls *Tumour*qmotif_4TTAGGGu.ini.xml`
+echo "id,includesN,unmappedN,includesT,unmappedT"
+
+#for f in `ls *PrimaryTumour*qmotif_4TTAGGGu.ini.xml`
+#for f in `ls *Tumour*qmotif_4TTAGGGu.ini.xml`
+for f in `ls *Tumour*qmotif_4TTAGGGc.ini.xml`
+#for f in `ls *tumour*_9TTAGGGc.qmotif.xml`
+#for f in `ls *tumour*_9AllVariantsu.qmotif.xml`
+do
+	id=`echo $f | sed -r "s/^[^_]*_([A-Z]*)([0-9]*)_.*$/\1_\2/"`
+	id2=`echo $f | sed -r "s/^[^_]*_([A-Z]*)([0-9]*)_.*$/\1\2/"`
+	includesT=`cat $f | grep "scaledIncludes" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
+	unmappedT=`cat $f | grep "scaledUnmapped" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
+	
+	n=`ls *$id2*Normal*qmotif_4TTAGGGc.ini.xml`
+	includesN=`cat $n | grep "scaledIncludes" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
+	unmappedN=`cat $n | grep "scaledUnmapped" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
+
+	echo "$id,$includesN,$unmappedN,$includesT,$unmappedT"
+done
+
