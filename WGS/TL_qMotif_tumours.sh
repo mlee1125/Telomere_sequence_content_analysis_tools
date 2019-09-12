b7d0b839c438b99744ed42e8d9f803065081dfe8
Michael Lee <52021457+mlee1125@users.noreply.github.com>
2019-09-12 10:14:11 +1000

    Update TL_qMotif_tumours.sh

--- WGS/TL_qMotif_tumours.sh
+++ WGS/TL_qMotif_tumours.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-echo "id,includesN,unmappedN,includesT,unmappedT"
+echo "id,includes,unmapped"
 
-for f in `ls *tumour*qmotif_9TTAGGGu.qmotif.xml`
+for f in `ls *qmotif_9TTAGGGu.qmotif.xml`
 do
-	id=`echo $f | sed -r "s/^[^_]*_([A-Z]*)([0-9]*)_.*$/\1_\2/"`
-	id2=`echo $f | sed -r "s/^[^_]*_([A-Z]*)([0-9]*)_.*$/\1\2/"`
-	includesT=`cat $f | grep "scaledIncludes" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
-	unmappedT=`cat $f | grep "scaledUnmapped" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
+	id=`echo $f`
+	includes=`cat $f | grep "scaledIncludes" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
+	unmapped=`cat $f | grep "scaledUnmapped" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
 	
-	n=`ls *$id2*normal*qmotif_9TTAGGGc.qmotif.xml`
-	includesN=`cat $n | grep "scaledIncludes" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
-	unmappedN=`cat $n | grep "scaledUnmapped" | sed -r "s/.*count..([0-9]*)[^0-9]*/\1/"`
-
-	echo "$id,$includesN,$unmappedN,$includesT,$unmappedT"
+	echo "$id,$includes,$unmapped"
 done
 
